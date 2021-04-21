41e390c524e3930af2df3cbb247dd2980cdc8396
yue-qiu <canbaodetudou@gmail.com>
2021-04-21 18:10:05 +0800

    v1

--- goroutinePool.go
+++ goroutinePool.go
@@ -2,6 +2,7 @@ package goroutinePool
 
 import (
 	"errors"
+	"log"
 	"sync"
 	"time"
 )
@@ -18,6 +19,7 @@ type taskChan struct {
 
 type Pool struct {
 	MaxWorkerCount int
+	currentWorkerCount int
 	taskChans []*taskChan
 	MaxIdleWorkerTime time.Duration
 	stop bool
@@ -67,20 +69,63 @@ func (pool *Pool) clean() {
 
 // FIFO
 func (pool *Pool) getTaskChan() *taskChan {
-	var ch *taskChan
+	var taskCh *taskChan
 	pool.lock.Lock()
 	defer pool.lock.Unlock()
 
 	if len(pool.taskChans) > 0 {
-		ch = pool.taskChans[0]
+		taskCh = pool.taskChans[0]
 		pool.taskChans = pool.taskChans[1:]
 	} else {
-		ch = &taskChan{
+		taskCh = &taskChan{
 			ch: make(chan Task),
-			lastUsedTime: time.Now(),
 		}
-		pool.taskChans = append(pool.taskChans, ch)
 	}
 
-	return ch
+	return taskCh
+}
+
+func (pool *Pool) consume(taskCh *taskChan) {
+	// 为这个 taskChan 开启一个对应的 goroutine
+	go func() {
+		// 防止 task 运行时发生 panic 导致整个程序崩溃
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("task panic: %s\n", r)
+			}
+		}()
+
+		for {
+			if task, ok := <-taskCh.ch; ok {
+				taskCh.lastUsedTime = time.Now()
+				task.Handler(task.Params...)
+			} else {
+				break
+			}
+		}
+	}()
+}
+
+func (pool *Pool) Put(task Task) error {
+	if pool.stop {
+		return errors.New("THE POOL HAS BEEN STOPPED")
+	}
+
+	taskCh := pool.getTaskChan()
+	if len(pool.taskChans) < pool.MaxWorkerCount {
+		pool.consume(taskCh)
+		taskCh.ch <-task
+		pool.taskChans = append(pool.taskChans, taskCh)
+		return nil
+	}
+
+	return errors.New("TOO MANY TASKCHAN")
+}
+
+func (pool *Pool) Stop() {
+	pool.stop = true
+
+	for i := range pool.taskChans {
+		close(pool.taskChans[i].ch)
+	}
 }
--- goroutinePool_test.go
+++ goroutinePool_test.go
@@ -1,1 +1,94 @@
 package goroutinePool
+
+import (
+	"log"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPoolPut(t *testing.T) {
+	pool := NewGoroutinePool(100, time.Second)
+	err := pool.Serve()
+	if err != nil {
+		t.Error()
+	}
+
+	lock := sync.Mutex{}
+	handler := func(params ...interface{}) {
+		a, b := 0, 1
+		for i :=0; i < (params[0]).(int); i++ {
+			a, b = b, a + b
+		}
+
+		lock.Lock()
+		res := (params[1]).(*[]int)
+		*res = append(*res, b)
+		lock.Unlock()
+	}
+	res := make([]int, 0)
+
+	for i := 0; i < 100; i++ {
+		task := Task{
+			Handler: handler,
+			Params: []interface{}{i, &res},
+		}
+
+		err = pool.Put(task)
+		if err!= nil {
+			t.Error()
+		}
+	}
+
+	log.Printf("%v\n", res)
+}
+
+func BenchmarkPool_Put(b *testing.B) {
+	pool := NewGoroutinePool(100, time.Second)
+	pool.Serve()
+	lock := sync.Mutex{}
+	handler := func(params ...interface{}) {
+		a, b := 0, 1
+		for i :=0; i < (params[0]).(int); i++ {
+			a, b = b, a + b
+		}
+
+		lock.Lock()
+		res := (params[1]).(*[]int)
+		*res = append(*res, b)
+		lock.Unlock()
+	}
+	res := make([]int, 0)
+
+	for i := 0; i < b.N; i++ {
+		task := Task{
+			Handler: handler,
+			Params: []interface{}{i, &res},
+		}
+
+		err := pool.Put(task)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func BenchmarkNormal(b *testing.B) {
+	lock := sync.Mutex{}
+	handler := func(params ...interface{}) {
+		a, b := 0, 1
+		for i :=0; i < (params[0]).(int); i++ {
+			a, b = b, a + b
+		}
+
+		lock.Lock()
+		res := (params[1]).(*[]int)
+		*res = append(*res, b)
+		lock.Unlock()
+	}
+	res := make([]int, 0)
+
+	for i := 0; i < b.N; i++ {
+		go handler([]interface{}{i, &res}...)
+	}
+}
